26f4a5f6e3d804462019282b532f4eb67155e427
Yuliana Rahman <rahman@mediainfo.co.id>
2012-12-11 01:08:06 +0700

    Update lgs/cron/autoassign.php

--- lgs/cron/autoassign.php
+++ lgs/cron/autoassign.php
@@ -1,13 +1,16 @@
 <?php
-// include("/var/www/lgs/config.php");
-define("SQL_IP", "localhost");
-  define("SQL_USER", "sindu");
-  define("SQL_PWD","blogsindu");
-  define("SQL_DATABASE","lgs");
+/*
+This file is for cron auto assignment (Advance only)
+*/
+include(str_replace("cron","",realpath(__FILE__))."config.php");
+//define("SQL_IP", "localhost");
+//  define("SQL_USER", "root");
+//  define("SQL_PWD","");
+//  define("SQL_DATABASE","lgs");
 mysql_connect(SQL_IP,SQL_USER,SQL_PWD);
-mysql_select_db("lgs");
+mysql_select_db(SQL_DATABASE);
 $date = date("Y-m-d H:i:s");
 $query = "UPDATE assignments SET status='1' WHERE status='0' AND start_time <= '".$date."' AND start_time!='0000-00-00 00:00:00'";
 mysql_query($query);
-// echo SQL_USER;
+
 ?>
